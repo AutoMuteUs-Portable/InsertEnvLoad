757ce76b4cd5b9ec6ad28147e213547a4b8c3585
mtaku3 <m.taku3.1222@gmail.com>
2021-02-19 22:25:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertEnvLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] allLines = File.ReadAllLines("main.go");
+            List<string> newLines = new List<string>();
+
+            for (var i = 0; i < allLines.Length; i++)
+            {
+                newLines.Add(allLines[i]);
+                if (allLines[i].Contains("func main()"))
+                {
+                    newLines.Add("  _loadErr := load()");
+                    newLines.Add("  if (_loadErr != nil) {");
+                    newLines.Add("      log.Fatal(_loadErr)");
+                    newLines.Add("  }");
+                }
+            }
+
+            File.WriteAllLines("main.go", newLines);
+        }
+    }
+}
